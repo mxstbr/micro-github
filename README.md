6f80f9b949a25e0937ef8c414c46f05aa3a8ade5
Max <contact@mstoiber.com>
2017-02-04 23:20:59 +0100

    Add error handling

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ A tiny microservice to add authentication with GitHub to your application.
 Running your own `micro-github` is a single [`now`](https://now.sh) command away:
 
 ```sh
-now -e GH_CLIENT_ID=xyz123 -e GH_CLIENT_SECRET=asdf123 -e REDIRECT_URL=https://google.com mxstbr/micro-github # Deploy this repository using now.sh
+# Deploy this repository using now.sh
+now mxstbr/micro-github -e GH_CLIENT_ID=xyz123 -e GH_CLIENT_SECRET=asdf123 -e REDIRECT_URL=https://google.com
 ```
 
 ### Environment variables
@@ -18,9 +19,9 @@ You'll need to provide three environment variables when running `micro-github`:
 # Your GitHub application client id
 GH_CLIENT_ID=xyz123
 # Your GitHub application client secret
-GH_CLIENT_SECRET=xyz123
+GH_CLIENT_SECRET=asdf123
 # The URL to redirect the user to once the authentication was successful
-REDIRECT_URL=xyz123
+REDIRECT_URL=https://google.com
 ```
 
 > Create an application on GitHub [here](https://github.com/settings/applications/new) to get your client id and secret if you haven't done that already.
@@ -42,6 +43,10 @@ https://github.com/login/oauth/authorize?client_id=asdf123
 Adding the client id that you got from GitHub as the `client_id` query param. (instead of `asdf123` in the example above)
 
 Then whatever you specified as the `REDIRECT_URL` will be called with the access token from GitHub for you to use! 🎉
+
+### Error handling
+
+In case an error happens (either by the service or on GitHub) the user will be redirected to the `REDIRECT_URL` with the `error` query param set to a relevant error message.
 
 ## Development
 
--- index.js
+++ index.js
@@ -4,41 +4,49 @@ const querystring = require('querystring')
 const axios = require('axios')
 const { send } = require('micro')
 
-const createRedirectHTML = (token) => `
+const createRedirectHTML = (data) => `
 <html>
 	<head>
 		<title>Redirecting…</title>
 	</head>
 	<body>
 		<script>
-			window.location.href = '${process.env.REDIRECT_URL}?access_token=${token}';
+			window.location.href = '${process.env.REDIRECT_URL}?${querystring.stringify(data)}';
 		</script>
 	</body>
 </html>
 `
 
 module.exports = async (req, res) => {
+	res.setHeader('Content-Type', 'text/html')
 	const { query: { code } } = url.parse(req.url, true)
 	if (!code) {
-		send(res, 401, 'Unauthorized')
+		send(res, 401, createRedirectHTML({ error: 'Provide code query param' }))
 	} else {
-		const { status, data } = await axios({
-			method: 'POST',
-			url: 'https://github.com/login/oauth/access_token',
-			responseType: 'json',
-			data: {
-				client_id: process.env.GH_CLIENT_ID,
-				client_secret: process.env.GH_CLIENT_SECRET,
-				code,
-			},
-		})
+		try {
+			const { status, data } = await axios({
+				method: 'POST',
+				url: 'https://github.com/login/oauth/access_token',
+				responseType: 'json',
+				data: {
+					client_id: process.env.GH_CLIENT_ID,
+					client_secret: process.env.GH_CLIENT_SECRET,
+					code,
+				},
+			})
 
-		if (status === 200) {
-			const qs = querystring.parse(data)
-			res.setHeader('Content-Type', 'text/html');
-			send(res, 200, createRedirectHTML(qs.access_token))
-		} else {
-			send(res, 401, 'Unauthorized')
+			if (status === 200) {
+				const qs = querystring.parse(data)
+				if (qs.error) {
+					send(res, 401, createRedirectHTML({ error: qs.error_description }))
+				} else {
+					send(res, 200, createRedirectHTML({ access_token: qs.access_token }))
+				}
+			} else {
+				send(res, 500, createRedirectHTML({ error: 'GitHub server error.' }))
+			}
+		} catch (err) {
+			send(res, 500, createRedirectHTML({ error: 'Please provide GH_CLIENT_ID and GH_CLIENT_SECRET as environment variables. (or GitHub might be down)' }))
 		}
 	}
 }
